647daaafc14406387f5f70d5c0a5f3feaa6c4e10
Kehvarl <kehvarl@kehvarl.com>
2024-03-21 19:58:43 -0700

    Some main menu stuff
    Animating the banner would be cool

--- app/main.rb
+++ app/main.rb
@@ -4,14 +4,25 @@ def setup args
 end
 
 def main_menu args
+  text_color = {r:255, g:255, b:255}
+  out = []
   # Draw Banner
+  out << {x: 500, y: 650, text: "Super Short Stuff", size_enum: 4, r: 255}.label!
+  out << {x: 507, y: 647, text: "Super Short Stuff", size_enum: 3, r: 255, g: 128, b:0}.label!
+  out << {x: 514, y: 644, text: "Super Short Stuff", size_enum: 2, r: 255, g: 255, b:0}.label!
+  out << {x: 521, y: 641, text: "Super Short Stuff", size_enum: 1, r: 255, g: 255, b:255}.label!
+
   # Draw Menu Items
+  out << {x:540, y:500, text: "1: New Game", **text_color}.label!
+  out << {x:540, y:470, text: "2: Blank", **text_color}.label!
   # Track Selected Menu Item
   # Change select with mouse or keyboard
     # Keyboard: Track selected index and increment/decrement
     # Mouse:  On mouse over label, select index
   # On click/enter do something
   # Maybe animate some background stuff
+
+  args.outputs.primitives << out
 end
 
 def game_over args
