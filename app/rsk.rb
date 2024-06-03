78bd54d8a2d9c21fbd5136837905203cc3a109eb
Kehvarl <kehvarl@kehvarl.com>
2024-06-02 21:10:14 -0700

    Animated cat sprite

--- app/rsk.rb
+++ app/rsk.rb
@@ -14,14 +14,72 @@ require('app/game.rb')
   # pounce 7
   # arch right 8
 
+class Cat
+  attr_sprite
+
+  def initialize (x,y)
+    @x = x
+    @y = y
+    @w = 64
+    @h = 64
+    @path= "sprites/sheets/cat.png"
+    @angle= 0
+    @tile_x=  0
+    @tile_y=  0
+    @tile_w= 32
+    @tile_h= 32
+    @flip_vertically = false
+    @flip_horizontally = false
+
+    @current_pose = :idle_lick
+    @current_frame = 0
+    @frame_duration = 10
+    @frame_delay = 10
+    @countdown = 0
+    @pose_list = {
+      sit_down: [0,4,4, [:sit_down, :idle_lick, :idle_sleep]],
+      sit_right: [1,4,4, [:sit_right, :idle_lick, :idle_sleep]],
+      idle_lick: [2,4,2, [:idle_lick, :idle_ear, :idle_sleep, :arch]],
+      idle_ear: [3,4,1, [:idle_lick, :idle_sleep, :arch]],
+      prance: [4,8,1, [:sit_down]],
+      leap: [5,8,1, [:sit_down]],
+      idle_sleep: [6,4,4, [:idle_sleep, :idle_ear, :idle_sleep, :arch]],
+      walk: [7,6,1, [:sit_down]],
+      pounce: [8,7,1, [:sit_down]],
+      arch: [9,8,1, [:sit_down]]
+    }
+  end
+
+  def tick args
+    @frame_duration -= 1
+    if @frame_duration <= 0
+      @frame_duration = @frame_delay
+      @current_frame += 1
+      if @current_frame >= @pose_list[@current_pose][1]
+        @current_frame = 0
+        @countdown -= 1
+        if @countdown <= 0
+          @current_pose = @pose_list[@current_pose][3].sample()
+          @countdown = @pose_list[@current_pose][2]
+        end
+      end
+    end
+    @tile_x = @current_frame*32
+    @tile_y = @pose_list[@current_pose][0]*32
+  end
+end
+
 class Rsk < Game
   def initialize args={}
     @robot = {x:632, y:352, w:16, h:32, angle:0}
+    @cat = Cat.new(200,100)
     @entities = []
   end
 
   def tick args
     super(args)
+
+    @cat.tick args
 
     if args.inputs.keyboard.key_held.up
       @robot.y += 1
@@ -40,8 +98,9 @@ class Rsk < Game
   end
 
   def render
-    out = {**@robot, path:"sprites/circle/indigo.png"}.sprite
-
+    out = []
+    out << {**@robot, path:"sprites/circle/indigo.png"}.sprite!
+    out << @cat
     out
   end
 end
