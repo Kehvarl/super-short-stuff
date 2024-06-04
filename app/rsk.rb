7206973bbecfe4dda17ce815de6611e063743c37
Kehvarl <kehvarl@kehvarl.com>
2024-06-03 19:20:50 -0700

    Inherit Sprite Tick Crab Sprite

--- app/rsk.rb
+++ app/rsk.rb
@@ -1,20 +1,51 @@
 require('app/game.rb')
 
-# Spritesheets
-# cat
-  # 32x32
-  # sit, down 4
-  # sit, right 4
-  # idle lick right 4
-  # idle earwash right 4
-  # prance right 8
-  # leap right 8
-  # sleep 4
-  # walk right 6
-  # pounce 7
-  # arch right 8
+class AnimSprite
+  attr_sprite
+  def initialize(x,y)
+    @x = x
+    @y = y
+    @w = 64
+    @h = 64
+    @path= "sprites/square/green.png"
+    @angle= 0
+    @tile_x=  0
+    @tile_y=  0
+    @tile_w= 32
+    @tile_h= 32
+    @flip_vertically = false
+    @flip_horizontally = false
 
-class Cat
+    @current_pose = :idle_lick
+    @current_frame = 0
+    @frame_duration = 10
+    @frame_delay = 10
+    @countdown = 0
+    @pose_list = {
+      idle: [0,1,1,[:idle]],
+    }
+  end
+
+  def tick(args)
+    @frame_duration -= 1
+    if @frame_duration <= 0
+      @frame_duration = @frame_delay
+      @current_frame += 1
+      if @current_frame >= @pose_list[@current_pose][1]
+        @current_frame = 0
+        @countdown -= 1
+        if @countdown <= 0
+          @current_pose = @pose_list[@current_pose][3].sample()
+          @countdown = @pose_list[@current_pose][2]
+        end
+      end
+    end
+    @tile_x = @current_frame*32
+    @tile_y = @pose_list[@current_pose][0]*32
+  end
+end
+
+class Cat < AnimSprite
   attr_sprite
 
   def initialize (x,y)
@@ -51,21 +82,42 @@ class Cat
   end
 
   def tick args
-    @frame_duration -= 1
-    if @frame_duration <= 0
-      @frame_duration = @frame_delay
-      @current_frame += 1
-      if @current_frame >= @pose_list[@current_pose][1]
-        @current_frame = 0
-        @countdown -= 1
-        if @countdown <= 0
-          @current_pose = @pose_list[@current_pose][3].sample()
-          @countdown = @pose_list[@current_pose][2]
-        end
-      end
-    end
-    @tile_x = @current_frame*32
-    @tile_y = @pose_list[@current_pose][0]*32
+    super args
+  end
+end
+
+class Crab < AnimSprite
+  attr_sprite
+
+  def initialize (x,y)
+    @x = x
+    @y = y
+    @w = 64
+    @h = 64
+    @path= "sprites/sheets/crab.png"
+    @angle= 0
+    @tile_x=  0
+    @tile_y=  0
+    @tile_w= 32
+    @tile_h= 32
+    @flip_vertically = false
+    @flip_horizontally = false
+
+    @current_pose = :idle
+    @current_frame = 0
+    @frame_duration = 10
+    @frame_delay = 10
+    @countdown = 0
+    @pose_list = {
+      idle: [0,4,4, [:idle]],
+      walk: [1,4,1, [:idle]],
+      die: [2,4,1, [:idle]],
+      attack: [3,4,1, [:idle]]
+    }
+  end
+
+  def tick args
+    super(args)
   end
 end
 
@@ -73,6 +125,7 @@ class Rsk < Game
   def initialize args={}
     @robot = {x:632, y:352, w:16, h:32, angle:0}
     @cat = Cat.new(200,100)
+    @crab = Crab.new(250,100)
     @entities = []
   end
 
@@ -80,6 +133,7 @@ class Rsk < Game
     super(args)
 
     @cat.tick args
+    @crab.tick args
 
     if args.inputs.keyboard.key_held.up
       @robot.y += 1
@@ -101,6 +155,7 @@ class Rsk < Game
     out = []
     out << {**@robot, path:"sprites/circle/indigo.png"}.sprite!
     out << @cat
+    out << @crab
     out
   end
 end
